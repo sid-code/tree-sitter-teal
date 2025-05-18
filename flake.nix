21703ddce0c1e5d7374a1b024ce98c466de50a6f
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2025-05-17 19:24:57 -0700

    flakeify

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Tree-sitter grammer for Teal";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      packages = forEachSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.tree-sitter.buildGrammar {
+            language = "teal";
+            version = "0.0.0";
+            src = ./.;
+          };
+        }
+      );
+    };
+}
